57eaf982a954f8ab9fa5d6ac67a97e53dfb75ef3
takahashi <shigeki.takahashi.dev@gmail.com>
2020-04-28 01:00:21 +0900

    Initial commit

---
+++ 01_movie2jpg.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+[summary]
+  Webカメラ、動画の各フレームをjpgとして保存する
+[description]
+  -
+"""
+
+import os
+import argparse
+import time
+
+import cv2 as cv
+
+
+def get_args():
+    """
+    [summary]
+        引数解析
+    Parameters
+    ----------
+    None
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--filepath", help='camera device number or movie path', default=0)
+    parser.add_argument(
+        "--resize_rate", help='resize rate', type=float, default=1.0)
+    parser.add_argument("--cwidth", help='capture width', type=int)
+    parser.add_argument("--cheight", help='capture height', type=int)
+    parser.add_argument(
+        "--display", help='display imshow', type=bool, default=True)
+    parser.add_argument("--fps", help='capture fps', type=int, default=30)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    """
+    [summary]
+        main()
+    Parameters
+    ----------
+    None
+    """
+    # 引数解析 #################################################################
+    args = get_args()
+    filepath = args.filepath
+    resize_rate = args.resize_rate
+    is_display = args.display
+    fps = args.fps
+
+    # カメラ or 動画準備 #######################################################
+    cap = cv.VideoCapture(filepath)
+    if args.cwidth is not None:
+        cap.set(cv.CAP_PROP_FRAME_WIDTH, args.cwidth)
+    if args.cheight is not None:
+        cap.set(cv.CAP_PROP_FRAME_HEIGHT, args.cheight)
+
+    # 保存先準備 ###############################################################
+    path_save_image = os.path.join('jpg')
+    os.makedirs(path_save_image, exist_ok=True)
+
+    # 画像ファイル名用番号
+    capture_count = len(os.listdir(path_save_image))
+
+    while True:
+        start_time = time.time()
+
+        # フレーム取得 #########################################################
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_h, frame_w = frame.shape[:2]
+        resize_frame = cv.resize(
+            frame, (int(frame_w * resize_rate), int(frame_h * resize_rate)))
+
+        # jpg画像書き込み ######################################################
+        path_image_file = os.path.join(path_save_image,
+                                       '{:06}.jpg'.format(capture_count))
+        cv.imwrite(path_image_file, resize_frame)
+
+        capture_count += 1
+
+        # 書き込み画像描画 ######################################################
+        if is_display:
+            cv.imshow('movie2jpg', resize_frame)
+
+        # FPS調整 ##############################################################
+        elapsed_time = time.time() - start_time
+        sleep_time = int((1000 / fps) - (elapsed_time * 1000))
+        if sleep_time <= 0:
+            sleep_time = 1
+
+        # キー入力(ESC:プログラム終了) #########################################
+        key = cv.waitKey(sleep_time)
+        if key == 27:  # ESC
+            break
+
+
+if __name__ == '__main__':
+    main()
---
+++ 02_jpg2movie.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+[summary]
+  Webカメラ、動画の各フレームをjpgとして保存する
+[description]
+  -
+"""
+
+import os
+import argparse
+import time
+
+import cv2 as cv
+
+
+def get_args():
+    """
+    [summary]
+        引数解析
+    Parameters
+    ----------
+    None
+    """
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--filepath", help='camera device number or movie path', default='jpg')
+    parser.add_argument(
+        "--resize_rate", help='resize rate', type=float, default=1.0)
+    parser.add_argument(
+        "--display", help='display imshow', type=bool, default=False)
+    parser.add_argument("--fps", help='capture fps', type=int, default=30)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    """
+    [summary]
+        main()
+    Parameters
+    ----------
+    None
+    """
+    # 引数解析 #################################################################
+    args = get_args()
+    filepath = args.filepath
+    resize_rate = args.resize_rate
+    is_display = args.display
+    fps = args.fps
+
+    # ファイル読み込み準備 ######################################################
+    cap = cv.VideoCapture(os.path.join(filepath, '%06d.jpg'))
+
+    # 保存先準備 ###############################################################
+    path_save_movie = os.path.join('movie')
+    os.makedirs(path_save_movie, exist_ok=True)
+
+    # ファイル名用番号
+    capture_count = len(os.listdir(path_save_movie))
+
+    # ビデオライター準備 ########################################################
+    ret, frame = cap.read()
+    if not ret:
+        exit()
+    frame_h, frame_w = frame.shape[:2]
+
+    fourcc = cv.VideoWriter_fourcc(*"DIVX")
+    writer = cv.VideoWriter(
+        os.path.join(path_save_movie, '{:06}.avi'.format(capture_count)),
+        fourcc, fps, (int(frame_w * resize_rate), int(frame_h * resize_rate)))
+
+    # 1フレーム目書き込み #######################################################
+    resize_frame = cv.resize(
+        frame, (int(frame_w * resize_rate), int(frame_h * resize_rate)))
+    writer.write(resize_frame)
+
+    while True:
+        start_time = time.time()
+
+        # フレーム取得 #########################################################
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_h, frame_w = frame.shape[:2]
+        resize_frame = cv.resize(
+            frame, (int(frame_w * resize_rate), int(frame_h * resize_rate)))
+
+        # フレーム書き込み #######################################################
+        writer.write(resize_frame)
+
+        if is_display:
+            cv.imshow('movie2jpg', resize_frame)
+
+            # FPS調整 ###########################################################
+            elapsed_time = time.time() - start_time
+            sleep_time = int((1000 / fps) - (elapsed_time * 1000))
+            if sleep_time <= 0:
+                sleep_time = 1
+
+            # キー入力(ESC:プログラム終了) #######################################
+            key = cv.waitKey(sleep_time)
+            if key == 27:  # ESC
+                break
+
+    writer.release()
+    cap.release()
+
+
+if __name__ == '__main__':
+    main()
